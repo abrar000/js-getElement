9e254df1a5b3393638091dc7eeb8ab1993307a98
abrar000 <abrar@uconnect.pk>
2019-01-02 10:55:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+// // document.getElementById
+// const name= document.getElementById('abrar');
+ 
+// //styling
+// name.style.background="red";
+// name.style.color="white";
+// name.textContent="wazir abrar hussain"; // to change the content
+// name.innerHTML='<span style="color:green"> Mr.waqar aly khan </span>';
+
+
+// document.querySelector()
+
+// console.log(document.querySelector('#abrar'));
+// console.log(document.querySelector('.para'));
+// console.log(document.querySelector('h1'));
+
+document.querySelector('h1').style.color = 'red';
+document.querySelector('p').style.color = 'green';
+
+console.log(document);
